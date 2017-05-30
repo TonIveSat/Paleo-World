8907bc6b6b55ad7baf333e07ed6a20293154f717
TonIveSat <tonny.sato@gmail.com>
2017-05-30 23:50:20 +0200

    Cursor Affordance

---
+++ Assets/Camera & UI/CursorAffordance.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour
+{
+    [SerializeField]
+    Texture2D WalkCursor = null;
+
+    [SerializeField]
+    Texture2D CombatCursor = null;
+
+    [SerializeField]
+    Texture2D StopCursor = null;
+
+    CameraRaycaster cameraRayCaster;
+    private Layer LastLayerHit;
+
+    // Use this for initialization
+    void Start ()
+    {
+        cameraRayCaster = GetComponent<CameraRaycaster>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        var currentLayerHit = cameraRayCaster.currentLayerHit;
+        if (currentLayerHit != LastLayerHit)
+        {
+            switch (currentLayerHit)
+            {
+                case Layer.Enemy:
+                    Cursor.SetCursor(CombatCursor, new Vector2(), CursorMode.Auto);
+                    break;
+                case Layer.RaycastEndStop:
+                    Cursor.SetCursor(StopCursor, new Vector2(), CursorMode.Auto);
+                    break;
+                case Layer.Walkable:
+                    Cursor.SetCursor(WalkCursor, new Vector2(), CursorMode.Auto);
+                    break;
+            }
+            LastLayerHit = currentLayerHit;
+        }
+    }
+}
