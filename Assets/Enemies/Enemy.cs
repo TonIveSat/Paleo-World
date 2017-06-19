90072f3c0cd8929258bb8639fa92af1f60b942e0
TonIveSat <tonny.sato@gmail.com>
2017-06-20 00:00:09 +0200

    terrain v1

---
+++ Assets/Enemies/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float currentHealthPoints;
+
+    [SerializeField]
+    float maxHealthPoints;
+
+    public float healthAsPercentage
+    {
+        get { return currentHealthPoints / maxHealthPoints; } 
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        currentHealthPoints = 80f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
